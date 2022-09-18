f011f3d205711595c475dea0f54dfcc18cd5b002
GeorgiCholev <georgi.k.cholev@gmail.com>
2022-09-18 17:30:04 +0300

    18th Sep 2022

---
+++ 07.MySQL/01.Data_Types_Data_Definition/Lab/p02_InsertData.sql
@@ -0,0 +1,11 @@
+USE `gamebar`;
+
+INSERT INTO `employees` (`first_name`, `last_name`) 
+VALUES 
+('Alex', 'Alexov'),
+('Bobi', 'Bobiev'),
+('Ceco', 'Cecov');
+
+INSERT INTO `employees` (`first_name`, `last_name`)
+VALUES
+('Denis', 'Denisov');
