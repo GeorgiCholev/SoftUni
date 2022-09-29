177058c80cc6821f4520225594d3ddd73a150219
GeorgiCholev <georgi.k.cholev@gmail.com>
2022-09-29 19:48:17 +0300

    29 Sep 2022

--- 07.MySQL/05.Table_Relations/lab.sql
+++ 07.MySQL/05.Table_Relations/lab.sql
@@ -46,12 +46,3 @@ CREATE TABLE `peaks` (
     REFERENCES `mountains`(`id`)
     ON DELETE CASCADE);
     
--- p05 Project Management DB
-CREATE TABLE `clients` (
-	`id` INT PRIMARY KEY AUTO_INCREMENT,
-    `client_name` VARCHAR(100));
-    
-CREATE TABLE `employees` (
-	`id` INT PRIMARY KEY AUTO_INCREMENT,
-    `first_name` VARCHAR(30),
-    `last_name` VARCHAR(30)); 
